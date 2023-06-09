7e787f9a206d518d7c8cea0311634fc3d571eb81
yunho <kimyouknow@naver.com>
2023-06-10 02:36:16 +0900

    Feat: Private Routes

---
+++ src/router/PrivateRoutes.tsx
@@ -0,0 +1,9 @@
+import PrivateRouter from '~/hoc/PrivateRouter';
+import { routesWithHeaderPrivate } from '~/router/routerInfo';
+
+export const privateRoutesWithHeader = routesWithHeaderPrivate.map(
+  ({ path, element }) => ({
+    path,
+    element: <PrivateRouter Component={element} />,
+  }),
+);
