49072d093def58b82bd68f0886fa0b589b03e9bb
yunho <kimyouknow@naver.com>
2023-06-11 03:05:57 +0900

    Feat: useRouter - refreshWindow

--- src/hooks/useRouter.ts
+++ src/hooks/useRouter.ts
@@ -3,8 +3,13 @@ import { useNavigate } from 'react-router-dom';
 export const useRouter = () => {
   const navigate = useNavigate();
 
+  const refreshWindow = () => {
+    location.reload();
+  };
+
   return {
     currentPath: window.location.pathname,
     routeTo: (path: string) => navigate(path),
+    refreshWindow,
   };
 };
