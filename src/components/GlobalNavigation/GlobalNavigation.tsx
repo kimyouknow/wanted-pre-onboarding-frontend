308555be42d50ceb1f44e5d0377b84eebbceb6f7
yunho <kimyouknow@naver.com>
2023-06-10 12:57:40 +0900

    Feat: global navigation

---
+++ src/components/GlobalNavigation/GlobalNavigation.tsx
@@ -0,0 +1,33 @@
+import { NavLink } from 'react-router-dom';
+
+import { gnbLinks } from '~/router/routerInfo';
+
+interface GlobalNavigationProps {
+  className?: string;
+}
+
+const GlobalNavigation = ({ className }: GlobalNavigationProps) => {
+  const isLogin = true; // TODO 판단 여부 함수로 변경하기
+  const links = gnbLinks(isLogin);
+  return (
+    <nav className={className}>
+      <ul className="flex flex-col gap-4">
+        {links.map(({ label, path }) => (
+          <li
+            key={path}
+            className="w-24 font-semibold text-gray-800 hover:text-blue-500"
+          >
+            <NavLink
+              to={path}
+              className={({ isActive }) => (isActive ? 'text-blue-500' : '')}
+            >
+              {label}
+            </NavLink>
+          </li>
+        ))}
+      </ul>
+    </nav>
+  );
+};
+
+export default GlobalNavigation;
