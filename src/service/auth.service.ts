77016ca98a977501671ed8d3210ac2a51607b8d9
yunho <kimyouknow@naver.com>
2023-06-12 10:44:07 +0900

    Feat: authService.getToken

--- src/service/auth.service.ts
+++ src/service/auth.service.ts
@@ -3,6 +3,7 @@ import browserStorage from '~/utils/browserStorage';
 const authService = {
   isLogin: () => !!browserStorage.get('token'),
   saveToken: (token: string) => browserStorage.set('token', token),
+  getToken: () => browserStorage.get('token'),
   logout: () => browserStorage.remove('token'),
 };
 
