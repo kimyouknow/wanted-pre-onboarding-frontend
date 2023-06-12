9c8f328d87a4e87ac5d5db51f8d320082e7acc2b
yunho <kimyouknow@naver.com>
2023-06-12 16:20:41 +0900

    Feat: TodoElement에서 체크박스 로직 추가

--- src/modules/TodoElement/TodoElement.tsx
+++ src/modules/TodoElement/TodoElement.tsx
@@ -1,13 +1,45 @@
+import { ChangeEvent, useState } from 'react';
+
+import todoApi from '~/api/domain/todoApi';
+import { useTodoProviderAction } from '~/context/todo/useTodoContext';
 import { TodoModel } from '~/types/todo.type';
 
 // eslint-disable-next-line @typescript-eslint/no-empty-interface
 export interface TodoElementProps extends TodoModel {}
 
-const TodoElement = ({ id, todo, isCompleted, userId }: TodoElementProps) => {
+const TodoElement = ({
+  id,
+  todo,
+  isCompleted: initIsCompleted,
+  userId,
+}: TodoElementProps) => {
+  const [isCompleted, setIsCompleted] = useState(initIsCompleted);
+  const { updateTodo } = useTodoProviderAction();
+
+  const onClickCheckbox = async (event: ChangeEvent<HTMLInputElement>) => {
+    const newValue = event.target.value === 'on';
+    try {
+      const response = await todoApi.updateTodo({
+        id,
+        todo,
+        isCompleted: newValue,
+        userId,
+      });
+      updateTodo(response.data);
+      setIsCompleted(newValue);
+    } catch (error) {
+      alert(error);
+    }
+  };
+
   return (
     <li>
       <label>
-        <input type="checkbox" checked={isCompleted} />
+        <input
+          type="checkbox"
+          checked={isCompleted}
+          onChange={onClickCheckbox}
+        />
         <span>{todo}</span>
       </label>
     </li>
